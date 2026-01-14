3bd4547146a54a7029835c903342801d4d6e72a5
vilma-cpu <vilma.goding.rask@gmail.com>
2026-01-14 18:44:18 +0000

    Uppgift 2 klar

---
+++ assignment_2.py
@@ -0,0 +1,8 @@
+# Uppgift 12
+
+#Skriv ett program som frågar "Vad heter du?" tar emot en sträng från tangentbordet, och skriver ut denna sträng 39 gånger, med nyrad emellan.
+
+name = input('Vad heter du?')
+
+for i in range (39):
+    print(name)
