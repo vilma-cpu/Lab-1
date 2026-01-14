4438877cebcb119a52589cd6806d6eda865f09df
vilma-cpu <vilma.goding.rask@gmail.com>
2026-01-14 18:31:27 +0000

    Fixade mallen till uppgift 1

--- assignment_1.py
+++ assignment_1.py
@@ -1,6 +1,7 @@
-"" Uppgift 1
+# Uppgift 1
 
-Skriv ett program som
+# Skriv ett program som
 
-    skapar en variabel nice_name och tilldelar den värdet "Kim", samt skriver ut värdet på denna variabeln
-    skapar en ny variabel long_name och tilldelar den värdet "KimKimKim..." (upprepat 33 gånger) genom att använda en for-slinga, samt skriver ut värdet på variabeln.""
+  #  skapar en variabel nice_name och tilldelar den värdet "Kim", samt skriver ut värdet på denna variabeln
+  #  skapar en ny variabel long_name och tilldelar den värdet "KimKimKim..." (upprepat 33 gånger) genom att använda en for-slinga, samt skriver ut värdet på variabeln.
+
