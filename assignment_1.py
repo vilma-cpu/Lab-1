6bda49997145ca1e1bf8b28c7c267738e5034e88
vilma-cpu <vilma.goding.rask@gmail.com>
2026-01-14 18:35:09 +0000

    Uppfigt 1 klar

--- assignment_1.py
+++ assignment_1.py
@@ -5,3 +5,10 @@
   #  skapar en variabel nice_name och tilldelar den värdet "Kim", samt skriver ut värdet på denna variabeln
   #  skapar en ny variabel long_name och tilldelar den värdet "KimKimKim..." (upprepat 33 gånger) genom att använda en for-slinga, samt skriver ut värdet på variabeln.
 
+nice_name = "Kim"
+print(nice_name)
+
+long_name = ""
+for i in range(33):
+    long_name += nice_name
+print(long_name)
